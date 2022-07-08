560f6af3dcf9655a879ed580c76b677026093351
Krzosa Karol <krzosa.karol@gmail.com>
2022-07-08 09:25:25 +0200

    Add required files from base

--- build.bat
+++ build.bat
@@ -1,11 +1,5 @@
 @echo off
 
-
-rem clang assets.cpp -Wall -Wno-unused-function -Wno-missing-braces -fno-exceptions -fdiagnostics-absolute-paths -g -I".." -o assets.exe -Wl,user32.lib
-rem assets.exe
-rem tracy/TracyClient.cpp -DTRACY_ENABLE
-
-
 pushd %~dp0
-clang main.cpp -mavx2 -Wall -Wno-unused-function -Wno-missing-braces -fno-exceptions -fdiagnostics-absolute-paths -g -I".." -o main.exe -Wl,user32.lib
+clang main.cpp -mavx2 -Wall -Wno-unused-function -Wno-missing-braces -fno-exceptions -fdiagnostics-absolute-paths -g -o main.exe -Wl,user32.lib
 popd
